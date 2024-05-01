0c1fa3beeedbc27b4b98e8dcb9f359c47594606f
luoaoxuan <915915818@qq.com>
2024-05-01 22:31:12 +0800

    5.1优化

--- src/modules/user/entities/user.entity.ts
+++ src/modules/user/entities/user.entity.ts
@@ -1,1 +1,19 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity({name:'mx_user'})
+export class User {
+    @PrimaryGeneratedColumn()
+    id:string
+    @Column()
+    email:string
+    @Column()
+    phone:string
+    @Column()
+    password:string
+    @Column()
+    wx_openid:string
+    @Column()
+    wx_unionid:string
+    @Column({nullable:true,comment:'创建时间',default:()=>"CURRENT_TIMESTAMP"})
+    create_time:Date
+}
